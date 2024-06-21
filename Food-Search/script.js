670919dc668e503d58c5d2cde1689cf7d590263f
Matixy <mateusz.zadrozny2004@wp.pl>
2024-06-21 13:46:52 +0200

    Update script.js

--- Food-Search/script.js
+++ Food-Search/script.js
@@ -1,5 +1,6 @@
 const foodsList = document.querySelectorAll('.food')
 const serachInput = document.querySelector('.search-input')
+const errorInfo = document.querySelector('.error-info')
 
 const main = () => {
   prepareDOMEvents()
@@ -18,6 +19,18 @@ const displayRightFood = (searchingValue) => {
       food.classList.add('hide')
     }
   })
+
+  toggleErrorInfo()
+}
+
+const toggleErrorInfo = () => {
+  visiblesFoods = [...foodsList].filter(food => food.classList.contains('hide'))
+
+  if (visiblesFoods.length === foodsList.length) {
+      errorInfo.textContent = "Sorry, we don't have food with this name"
+  } else {
+    errorInfo.textContent = ''
+  }
 }
 
 const prepareDOMEvents = () => {
